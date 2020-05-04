fe682badc58a830f0d2030a7398865b2e7338003
orozcoja <orozcoja@umich.edu>
2020-05-04 19:41:30 -0400

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Joanne Orozco on 5/4/20.
+//  Copyright © 2020 jojo_orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
